339bf2cff0767b6bece48b34f608b956e744c72b
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2018-01-19 17:47:31 +0000

    Learning Free vs Tagless!

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "free-tagless-compare"
+
+version := "1.0"
+
+scalaVersion := "2.12.4"
+
+libraryDependencies += "org.typelevel" %% "cats" % "0.9.0"
+
+addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.3")
+    
---
+++ src/main/scala/com/tutorial/free/Comparison.scala
@@ -0,0 +1,65 @@
+package com.tutorial.free
+
+import java.util.UUID
+
+import cats.free.Free
+import cats.~>
+import cats.implicits._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+case class User(id: UUID, email: String, loyaltyPoints: Int)
+
+object Original {
+
+  trait UserRepository {
+    def findUser(id: UUID): Future[Option[User]]
+    def updateUser(u: User): Future[Unit]
+  }
+
+  class LoyaltyPoints(ur: UserRepository) {
+    def addPoints(userId: UUID, pointsToAdd: Int): Future[Either[String, Unit]] = {
+      ur.findUser(userId).flatMap {
+        case None => Future.successful(Left("User not found"))
+        case Some(user) =>
+          val updated = user.copy(loyaltyPoints = user.loyaltyPoints + pointsToAdd)
+          ur.updateUser(updated).map(_ => Right())
+      }
+    }
+  }
+}
+
+object UsingFree {
+
+  sealed trait UserRepositoryAlg[T]
+  case class FindUser(id: UUID) extends UserRepositoryAlg[Option[User]]
+  case class UpdateUser(u: User) extends UserRepositoryAlg[Unit]
+
+  type UserRepository[T] = Free[UserRepositoryAlg, T]
+
+  def findUser(id: UUID): UserRepository[Option[User]] = Free.liftF(FindUser(id))
+  def updateUser(u: User): UserRepository[Unit] = Free.liftF(UpdateUser(u))
+
+  def addPoints(userId: UUID, pointsToAdd: Int): UserRepository[Either[String, Unit]] = {
+    findUser(userId).flatMap {
+      case None => Free.pure(Left("User not found"))
+      case Some(user) =>
+        val updated = user.copy(loyaltyPoints = user.loyaltyPoints + pointsToAdd)
+        updateUser(updated).map(_ => Right())
+    }
+  }
+
+  val futureInterpreter = new (UserRepositoryAlg ~> Future) {
+    override def apply[A](fa: UserRepositoryAlg[A]): Future[A] = fa match {
+      case FindUser(id) => Future.successful(None)
+      case UpdateUser(u) => Future.successful(())
+    }
+  }
+
+  val result: Future[Either[String, Unit]] = addPoints(UUID.randomUUID(), 10).foldMap(futureInterpreter)
+}
+
+object UsingTagless {
+
+}
